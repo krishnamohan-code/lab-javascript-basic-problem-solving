0f4d49e5bfccddb107eb7af15a97377b0fc6e953
krishnamohan <krishnamohan.py@gmail.com>
2020-05-19 11:38:52 +0530

    added

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if(len1>len2){
 // - If it contains vowels, print the number of name, and also print the vowel letters along with the vowel characters. or
 // - print no vowels
  /*************************************To find the vowels letters******************** */
- var i;
+ var i; //KrishnaMohan
  for(i=0;i<ProGrad_1.length;i++)
  {
      if(ProGrad_1[i].toLowerCase()=="a"||ProGrad_1[i].toLowerCase()=="i"||ProGrad_1[i].toLowerCase()=="e"||ProGrad_1[i].toLowerCase()=="o"||ProGrad_1[i].toLowerCase()=="u")
@@ -42,38 +42,29 @@ if(len1>len2){
  {
      if(ProGrad_2[i].toLowerCase()=="a"||ProGrad_2[i].toLowerCase()=="i"||ProGrad_2[i].toLowerCase()=="e"||ProGrad_2[i].toLowerCase()=="o"||ProGrad_2[i].toLowerCase()=="u")
      {
-         console.log("The vowel letter is "+ProGrad_1[i]);
+         console.log("The vowel letter is "+ProGrad_2[i]);
      }
  }
 
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
 // - Print the number of upper case characters
 // - Print the number of lower case characters
-/*****************Check the no of upper cases and lower cases *************** */
-var z=0,count=0,flag=0;
-for(i=0;i<ProGrad_1.length;i++){
-    var temp=ProGrad_1[i].charCodeAt();
-    for(var k=65,j=97;z<26;k++,j++,z++)
-    {
-        if(temp==k)
-        {
-             count+=1;
-             break;
-        }
-        if(temp==j){
-            flag+=1
-            break;
-        }
+count= 0;flag = 0;
+for (let i = 0; i < ProGrad_1.length; i++) {
+    char = ProGrad_1.charAt(i);
+    if (char == char.toUpperCase()) {
+        count += 1
     }
-    
+    else
+        if (char == char.toLowerCase()) {
+            flag += 1
+        }
 }
 console.log("the number of upper case characters "+count);
-console.log("the number of Lower case characters "+flag);
+console.log("the number of lower case characters "+flag);
 
 // Progression 3: Control Statements - 2
 // 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
-/****************Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"** */
-// ProGrad_1=prompt("Enter the string")
 var st=ProGrad_1.toUpperCase()
 for(var i=0;i<st.length;i++)
 { 
@@ -82,17 +73,14 @@ for(var i=0;i<st.length;i++)
 }
 
 // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
-/*******3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP" ***************/
+revstr=""
 for(var i=ProGrad_2.length-1;i>=0;i--){
-    console.log(ProGrad_2[i]);
+    revstr+=ProGrad_2[i]
 }
+console.log("The reverse order of navigator name is "+revstr);
 
 // 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
 // - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
-/*********** 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
-// - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad" */
-// ProGrad_1=prompt("Enter the driver's name ")
-// ProGrad_2=prompt("\tEnter the Navigator name")
 st=ProGrad_1.concat(ProGrad_2)
 st=st.slice(" ")
 console.log(st)
@@ -108,6 +96,16 @@ console.log(st)
 // Generate 3 paragraphs. Store the text in a variable type of string.
 // Make your program count the number of words in the string.
 // Make your program count the number of times the Latin word et appears.
+count=0
+var string=prompt("Enter the string")
+for (x of string){
+    if(x==" "){
+        count+=1
+    }
+    
+}
+count+=1
+console.log("No of words in the string is "+count);
 // Bonus 2:
 // Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
 
@@ -122,27 +120,7 @@ console.log(st)
 
 // Hint: If you use Google to help you to find solution to this iteration, you might run into some solutions that use advanced string or array methods (such as join(), reverse(), etc.). However, try to apply the knowledge you currently have since you can build pretty nice solution with just using for loop, if-else statements with some break and continue... Just sayin' 
 
-   
 
-
-
-
-/*************Bonus Time!
-// Bonus 1:
-// Go to lorem ipsum generator and:
-// Generate 3 paragraphs. Store the text in a variable type of string.
-// Make your program count the number of words in the string.
-// Make your program count the number of times the Latin word et appears.******* */
-count=0
-var string=prompt("Enter the string")
-for (x of string){
-    if(x==" "){
-        count+=1
-    }
-    
-}
-count+=1
-console.log("No of words in the string is "+count);
 
 var array=[]
 input=prompt("Enter the string")
